38afa627635a97d3cf97b1741721d0d4d51cf59f
Elvin Chu <elvincth@gmail.com>
2023-11-19 06:49:41 +0800

    wip: gpt api

--- apps/backend/src/env.ts
+++ apps/backend/src/env.ts
@@ -4,6 +4,12 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     PORT: z.string().default("3001"),
+    AZURE_OPENAI_API_KEY: z.string(),
+    AZURE_OPENAI_ENDPOINT: z.string(),
+    AZURE_OPENAI_DEPLOYMENT_NAME: z.string(),
+    AZURE_OPENAI_EMBEDDINGS_DEPLOYMENT_NAME: z.string(),
+    AZURE_OPENAI_INSTANCE_NAME: z.string(),
+    AZURE_OPENAI_API_VERSION: z.string(),
   },
   /**
    * What object holds the environment variables at runtime. This is usually
